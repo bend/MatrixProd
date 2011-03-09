1a3b08ac0b9f03209c75942260d87f460b717042
Benoit Daccache <ben.daccache@gmail.com>
2011-03-09 16:33:18 +0100

    test for matrix completed

--- testing/testing.c
+++ testing/testing.c
@@ -2,32 +2,39 @@
 #include <stdio.h>
 
 #include "../matrix.h"
-/*
+
 void
 test_create_matrix(){
 	matrix *matr;
-	assert(create_matrix(10,10, matr)==0);
+	matr = create_matrix(10,10);
+	assert(matr!=NULL);
+	assert(matr->n =10);
+	assert(matr->m=10);
 	free_matrix(matr);
 	
-	assert(create_matrix(100,100, matr)==0);
+	matr = create_matrix(100,100);
+	assert(matr!=NULL);
 	free_matrix(matr);
 	
-	assert(create_matrix(1000,1000, matr)==0);
+	matr = create_matrix(1000,100);
+	assert(matr!=NULL);
+	assert(matr->n=1000);
+	assert(matr->m=100);
 	free_matrix(matr);
+
 	
-	assert(create_matrix(3000,3000, matr)==0);
+	matr = create_matrix(3000,3000);
+	assert(matr!=NULL);
 	free_matrix(matr);
 }
-*/
+
 void
 test_set_matrix_elem_at(){
   	matrix *matr;
 	matr = create_matrix(1,1);
-	if(matr == NULL)
-	  printf("eror");
+	assert(matr!=NULL);
 	assert(set_matrix_elem_at(0,0,matr,5)==0);
 	assert(get_matrix_elem_at(0,0,matr)==5);
-
 	free_matrix(matr);
 }
 
