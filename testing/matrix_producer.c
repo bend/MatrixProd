c2f8b6169bada605b8d2e9ed3ed9ccdbe336f435
Bend <ben.daccache@gmail.com>
2011-03-22 11:54:50 +0100

    added matrix producer and file_writer

---
+++ testing/matrix_producer.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "../file_writer.h"
+
+void
+produce_matrices(FILE* f, unsigned int nb_lines, unsigned int nb_matrices){
+	unsigned int nb_columns;
+	unsigned int i,j,s;
+	if(nb_matrices == 0)
+		return ;
+	nb_columns = rand()%3000+1;
+	file_write_matrix_size(f, nb_lines, nb_columns);
+	for(i=0; i<nb_lines; i++){
+		for(j=0; j<nb_columns; j++){
+			s = rand()%2;
+			if(s == 0)
+				fprintf(f,"-");
+			if(j==nb_columns -1)
+				fprintf(f,"%d\n",rand()%127);
+			else
+				fprintf(f,"%d ",rand()%127);
+		}
+	}
+	produce_matrices(f,nb_columns,nb_matrices-1);
+}
+
+int main(int argc, char**argv){
+	if(argc<3){
+		printf("usage: matrixproducer nb_m nb_lines_first\n");
+		return -1;
+	}
+	FILE* f;
+	char path[] ="test_files/gen.txt";
+	file_wopen(&f,path);
+	produce_matrices(f,atoi(argv[1]), atoi(argv[2]));
+	return 1;
+}
---
+++ testing/test_write_file.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+
+
+void
+test_file_write(){
+}
+
+
+
+
+
+int
+main(){
+
+	printf("All write_file tests succeded\n");
+	return 1;
+}
