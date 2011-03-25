b9b387b8eee7b5c7b5fa1849049535236aeb3687
Bend <ben.daccache@gmail.com>
2011-03-25 17:08:28 +0100

    tests added for thread producer

--- testing/test_producer.c
+++ testing/test_producer.c
@@ -69,12 +69,58 @@ void test_producer_start(){
 	pthread_join(t,NULL);
 }
 
+void
+test_producer_thread(){
+	state *s;
+	producer *p;
+	pthread_t *t;
+	matrix *mat;
+	matrix *mat1,*mat2,*mat3;
+	int *ret_val;
+	int m1[] = {1,
+				1};
+	int m2[] = {2,2,
+				2,2};
+	int m3[] = {1,2,3,
+				1,2,3,
+				1,2,3};
+	assert(matrix_alloc(&mat1,2,1)==0);
+	assert(matrix_alloc(&mat2,2,2)==0);
+	assert(matrix_alloc(&mat3,3,3)==0);
+	assert(matrix_fill(mat1,m1)==0);
+	assert(matrix_fill(mat2,m2)==0);
+	assert(matrix_fill(mat3,m3)==0);
+	
+	assert(state_alloc(&s) == 0);
+	assert(producer_alloc(PATH,s,&p)==0);
+	producer_thread_start(p,&t);
+	
+	while(s->producer_finished==false ){
+		sem_wait(s->consumer_allowed_mutex);
+	 	
+		assert(linked_list_remove_first(s->ll, &mat)==0);
+		assert(matrix_cmp(mat,mat1)==0);
+		sem_post(s->can_produce_sem);
+	 	
+		sem_wait(s->consumer_allowed_mutex);
+		assert(linked_list_remove_first(s->ll, &mat)==0);
+		assert(matrix_cmp(mat,mat2)==0);
+		sem_post(s->can_produce_sem);
+	 	
+		sem_wait(s->consumer_allowed_mutex);
+		assert(linked_list_remove_first(s->ll, &mat)==0);
+		assert(matrix_cmp(mat,mat3)==0);
+		sem_post(s->can_produce_sem);
+	}
+	pthread_join(*t,(void**)&ret_val);
+}
 
 
 int
 main(){
 	test_producer_alloc();
 	test_producer_start();
+	test_producer_thread();
 	printf("All producer tests succeded\n");
 	return EXIT_SUCCESS;
 }
